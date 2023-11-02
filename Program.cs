5fb2e80e243ed9417dc07998715bcbdd721be07f
José Felipe Tavares Costa <t.jose.costa@estapar.com.br>
2023-11-02 17:20:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using CSharpOO;
+
+Pessoa pessoa1 = new Pessoa();
+Pessoa pessoa2 = new Pessoa();
+
+Console.WriteLine("Dados da primeira pessoa:");
+Console.Write("Nome: ");
+pessoa1.Nome = Console.ReadLine();
+Console.Write("Idade: ");
+pessoa1.Idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados da segunda pessoa:");
+Console.Write("Nome: ");
+pessoa2.Nome = Console.ReadLine();
+Console.Write("Idade: ");
+pessoa2.Idade = int.Parse(Console.ReadLine());
+
+Pessoa pessoaMaisVelha = pessoa1.Idade > pessoa2.Idade ? pessoa1 : pessoa2;
+Console.WriteLine($"Pessoa mais velha: {pessoaMaisVelha.Nome}");
