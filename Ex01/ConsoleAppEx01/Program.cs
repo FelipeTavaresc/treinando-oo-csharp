f117179a5c12c526f56ff259f5239c6416f026ee
Felipe Tavaresc <felipe.tavares@outlook.com>
2023-11-07 20:54:36 -0300

    exercício retângulo

---
+++ Ex01/ConsoleAppEx01/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleAppEx01;
+using System.Globalization;
+
+Console.WriteLine("Entre a largura e altura do retângulo:");
+
+Retangulo retangulo = new Retangulo();
+retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine($"AREA {retangulo.Area().ToString("F2")}");
+Console.WriteLine($"PERIMETRO {retangulo.Perimetro().ToString("F2")}");
+Console.WriteLine($"DIAGONAL {retangulo.Diagonal().ToString("F2")}");
