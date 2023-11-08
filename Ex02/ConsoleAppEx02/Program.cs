078e16d59641ac602c71cea281afa8ab091d9ce6
Felipe Tavaresc <felipe.tavares@outlook.com>
2023-11-07 21:16:08 -0300

    exercício cálculo salário funcionário

---
+++ Ex02/ConsoleAppEx02/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleAppEx02;
+using System.Globalization;
+
+Funcionario funcionario = new Funcionario();
+Console.Write("Nome: ");
+funcionario.Nome = Console.ReadLine();
+
+Console.Write("Salário Bruto: ");
+funcionario.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Imposto: ");
+funcionario.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Funcionário: " + funcionario);
+
+Console.Write("Digite a porcentagem para aumentar o salário: ");
+funcionario.AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+Console.WriteLine($"Dados atualizados: {funcionario}");
