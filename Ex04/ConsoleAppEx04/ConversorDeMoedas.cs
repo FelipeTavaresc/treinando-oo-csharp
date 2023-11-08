02d111dabdc74768c6d8b1a0e59323ed2c155e22
Felipe Tavaresc <felipe.tavares@outlook.com>
2023-11-08 20:26:07 -0300

    add classe static ConversorDeMoedas

---
+++ Ex04/ConsoleAppEx04/ConversorDeMoedas.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleAppEx04
+{
+    public class ConversorDeMoedas
+    {
+        private static double Iof = 6.0;
+        public static double CotacaoEmReais(double cotacao, double quantidadeDolares)
+        {
+            double dolarComIOF = quantidadeDolares + (quantidadeDolares * Iof) / 100;
+            return cotacao * dolarComIOF;
+        }
+    }
+}
---
+++ Ex04/ConsoleAppEx04/Program.cs
@@ -0,0 +1,10 @@
+﻿using ConsoleAppEx04;
+using System.Globalization;
+
+Console.Write("Qual é a cotação do dólar? ");
+double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantos dólares você vai comprar ? ");
+double quatidadeDolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine($"Valor a ser pago em reais = {ConversorDeMoedas.CotacaoEmReais(cotacao, quatidadeDolares).ToString("F2")}");
