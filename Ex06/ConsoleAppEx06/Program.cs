972bff309385d5f52c902c8c1845332e31054174
Felipe Tavaresc <felipe.tavares@outlook.com>
2023-11-15 16:06:35 -0300

    vetores com tipos de referência

---
+++ Ex06/ConsoleAppEx06/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleAppEx06;
+
+Console.Write("Quantos quartos serão alugados? ");
+int qtdQuartosQueSeraoAlugados = int.Parse(Console.ReadLine());
+Quarto[] quartos = new Quarto[10];
+
+Console.WriteLine();
+
+for (int i = 1; i <= qtdQuartosQueSeraoAlugados; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Aluguel #{i}:");
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine();
+    Console.Write("Email: ");
+    string email = Console.ReadLine();
+    Console.Write("Quarto: ");
+    int numeroQuarto = int.Parse(Console.ReadLine());
+
+    quartos[numeroQuarto] = new Quarto { Estudante = new Estudante(nome, email), NumeroQuarto = numeroQuarto };
+}
+Console.WriteLine();
+
+Console.WriteLine("Quartos Ocupados");
+for (int i = 0; i < quartos.Length; i++)
+{
+    if (quartos[i] != null)
+    {
+        Console.WriteLine($"{quartos[i].NumeroQuarto}: {quartos[i].Estudante.Nome}, {quartos[i].Estudante.Email}");
+    }
+}
---
+++ Ex06/ConsoleAppEx06/Quarto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEx06
+{
+    public class Quarto
+    {
+        public Estudante Estudante { get; set; }
+        public int NumeroQuarto { get; set; }
+    }
+}
